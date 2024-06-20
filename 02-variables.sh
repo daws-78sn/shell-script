9b89635d3e22153161f13fafba926950ff326053
joindevops <skreddy2244@gmail.com>
2024-06-20 23:05:47 +0530

    hello script

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,10 +2,10 @@
 
 #declaring varaible 
 person1=1 # no space b/w = and value
-PERSON2=2
+person2=2
 
 #referring variable
-echo "$PERSON1:: hello $PERSON2, how are you?"
-echo "$PERSON2:: Hi $person1, i am fine . how are you?"
-echo "$PERSON1:: i am fine ,How is you work? "
-echo "$PERSON2:: not bad , i am planning to learn devops"
+echo "$person1:: hello $person2, how are you?"
+echo "$person2:: Hi $person1, i am fine . how are you?"
+echo "$person1:: i am fine ,How is you work? "
+echo "$person2:: not bad , i am planning to learn devops"

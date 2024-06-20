52c0688443a56881aebba0cd81f00bb50504a3f3
joindevops <skreddy2244@gmail.com>
2024-06-21 01:28:14 +0530

    hello-script

--- 09-installation.sh
+++ 09-installation.sh
@@ -14,6 +14,16 @@ dnf install git -y
 
 if [ $? -ne 0 ]
 then
+    echo "Installation of git... FAILURE"
+    exit 1
+else
+    echo "Installation of git.. SUCCESS"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
     echo "Installation of mysql... FAILURE"
     exit 1
 else

60a1302b9cb4c8488df2cf1ed85989677a5fef20
joindevops <skreddy2244@gmail.com>
2024-06-20 23:32:08 +0530

    hello script

---
+++ 05-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTILLU" "MURARI")
+    # size of above array is 3
+    # index starts with 0 , 0 1 2 
+    # list always starts with 0
+
+echo "First movie is: ${MOVIES[0]}"
+echo "Second movie is: ${MOVIES[1]}"
+echo "Third movie is: ${MOVIES[2]}"

99012d3228937d7a8674886d1d99749d1df12043
joindevops <skreddy2244@gmail.com>
2024-06-20 23:20:33 +0530

    hello script

--- 04-variables.sh
+++ 04-variables.sh
@@ -8,7 +8,7 @@ echo "please enter your password::"
 
 read PASSWORD # here USERNAME is varaible and to hide the  user name and password while entering on shell we use -s after read
 
-echo "username is : $USERNAME, password is: $PASSWORD
+echo "username is : $USERNAME, password is: $PASSWORD"
 
 
 

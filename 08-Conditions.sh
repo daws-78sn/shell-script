26c72fb709f0ce87177a11d1c2bd9cc4212faafe
joindevops <skreddy2244@gmail.com>
2024-06-21 00:58:31 +0530

    hello-script

--- 08-Conditions.sh
+++ 08-Conditions.sh
@@ -7,7 +7,6 @@ then
     echo " Given number $NUMBER is greater than 10"
 else
     echo " Given number $NUMBER is less than 10"
-
 fi
 
 #-gt, -lt, -eq, -ge, -le , greater than, lessthan, equal to, 

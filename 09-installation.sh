edccd653e0c457f45aa88cb0553172fa97f123cc
joindevops <skreddy2244@gmail.com>
2024-06-21 01:20:57 +0530

    hello-script

--- 09-installation.sh
+++ 09-installation.sh
@@ -5,11 +5,18 @@ USERID=$(id -u)
 if [$USERID -ne 0]
 then
     echo "Please run this script as root user"
+    exit1 #manually exit if error comes
 else
     echo "You are super user."
 fi
 
 dnf install mysql -y
+
+if [$? -ne 0]
+then
+    echo "Installation of mysql... FAILURE"
+    exit1
+fi
 
 echo "is script proceeding?"
 #read -p "Press enter to continue..."

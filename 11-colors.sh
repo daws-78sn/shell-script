288ddbd644d7243106ff3bba2b18d4b63ea72abc
joindevops <skreddy2244@gmail.com>
2024-06-21 21:51:52 +0530

    hello-script

---
+++ 11-colors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m]
+
+echo "Script started ececuting at: $TIMESTAMP"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script as root user"
+    exit 1 #manually exit if error comes
+else
+    echo "You are super user."
+fi
+
+dnf install git -y &>>$LOGFILE
+
+VALIDATE $? "installing mygit" # $? gives the exit status of previous command
+
+
+dnf install mysql -y &>>LOGFILE
+
+VALIDATE $? "installing sql" # $? gives the exit status of previous command
+

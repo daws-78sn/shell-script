f25295e4995ddc211656f55b624a29470f0c62af
joindevops <skreddy2244@gmail.com>
2024-06-23 23:22:31 +0530

    expense

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -2,14 +2,17 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
+MESSAGE=""
 
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}'| cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE"
+        MESSAGE="$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE"
     fi
 
 done <<< $DISK_USAGE
+
+echo "Message: $MESSAGE"
     

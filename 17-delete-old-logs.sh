12c460af3080b44caf11179c946c42c99308fcf2
joindevops <skreddy2244@gmail.com>
2024-06-23 22:38:07 +0530

    expense

--- 17-delete-old-logs.sh
+++ 17-delete-old-logs.sh
@@ -15,10 +15,11 @@ else
     exit 1
 fi
 
-FILES= $(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 echo "Files to delete:$FILES"
 
 while IFS= read -r line
 do
     echo "Deleting file: $line"
+    rm -rf $line
 done <<<$FILES

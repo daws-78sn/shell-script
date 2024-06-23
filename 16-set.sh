85b02c8afe6f9bee640da32085537fac2148cb7b
joindevops <skreddy2244@gmail.com>
2024-06-23 20:21:56 +0530

    hello-script

---
+++ 16-set.sh
@@ -0,0 +1,17 @@
+#!/bin.bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script as root user"
+    exit 1 #manually exit if error comes
+else
+    echo "You are super user."
+fi
+
+dnf install gitt -y
+dnf install mysql -y
+echo "is script proceeding"
+
+
